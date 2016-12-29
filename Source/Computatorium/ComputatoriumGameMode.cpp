46c2b1f308657ad568a0082051e47b030109bde1
Adam Simpson <adambsimpson@gmail.com>
2016-12-29 10:26:06 -0500

    Refactor directory structure

--- Source/Computatorium/ComputatoriumGameMode.cpp
+++ Source/Computatorium/ComputatoriumGameMode.cpp
@@ -11,7 +11,7 @@ AComputatoriumGameMode::AComputatoriumGameMode()
 	PlayerControllerClass = AComputatoriumPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/TopDownCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
