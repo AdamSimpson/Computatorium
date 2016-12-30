d7a2c05afa09210d60ede353a59fec5375107da9
Adam Simpson <adambsimpson@gmail.com>
2016-12-30 07:05:17 -0500

    Begin system memory fetchable implimentation

---
+++ Source/Computatorium/SystemMemoryFetchable.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Computatorium.h"
+#include "SystemMemoryFetchable.h"
+
+
+ASystemMemoryFetchable::ASystemMemoryFetchable() {
+
+}
+
---
+++ Source/Computatorium/SystemMemoryFetchable.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Fetchable.h"
+#include "SystemMemoryFetchable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMPUTATORIUM_API ASystemMemoryFetchable : public AFetchable
+{
+	GENERATED_BODY()
+
+public:
+	ASystemMemoryFetchable();
+	
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Memory", meta = (AllowPrivateAccess = "true"))
+	int value;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Memory", meta = (AllowPrivateAccess = "true"))
+	int location;
+};
